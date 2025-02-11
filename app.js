0444787db68403dbdc0dc31d7ac97fad92eec886
petemurray45 <pmurray697@gmail.com>
2025-02-11 00:49:30 +0000

    added functionality for landing page

--- app.js
+++ app.js
@@ -2,18 +2,31 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
+app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'assets')));
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+const users = [
+    {email: 'pmurray697@gmail.com',
+     password: 'oldmill50',
+    }
+];
 
 app.get("/", (req, res) => {
-
     res.render("index");
-
 })
 
-app.get("/", (req, res) => {
+app.post('/login', (req, res)=> {
+    const {email, password} = req.body;
+    const user = users.find(u => u.email === email && u.password === password);
 
-    res.render("landing");
+    if (user){
+        res.render('landing', {user});
+    } else {
+        res.render('index', {error: 'Invalid email or password'});
+    }
 })
 
 app.listen(3000, (err)=>{
